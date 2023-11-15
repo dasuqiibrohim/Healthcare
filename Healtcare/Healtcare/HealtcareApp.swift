6618d23fc89ad1fe9dfc9085ad09dc13a9418f27
Ibrohim Dasuqi <ibrohim.dasuqi@efishery.com>
2023-11-15 20:57:38 +0700

    Commit first.

---
+++ Healtcare/Healtcare/HealtcareApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealtcareApp.swift
+//  Healtcare
+//
+//  Created by eFishery on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HealtcareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
